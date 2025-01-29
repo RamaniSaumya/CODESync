07fa525555109d99591d683168275f0f9da40ef4
Saumya Ramani <raman@Saumya.com>
2025-01-29 17:58:52 +0530

    final hide of data

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,18 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth"
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: env.apiKey,
-  authDomain:env.authDomain,
-  projectId: env.projectId,
-  storageBucket: env.storageBucket,
-  messagingSenderId: env.messagingSenderId,
-  appId: env.appId
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY, // or NEXT_PUBLIC_FIREBASE_API_KEY
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
-
-export const auth=getAuth();
+export const auth = getAuth();
 export default app;
